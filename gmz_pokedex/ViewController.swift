85a936c4e4fd9ee8afa912093616832638fd69ea
melgomez <meldarrelgomez@gmail.com>
2017-10-10 22:10:14 +0800

    First Commit.

---
+++ gmz_pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gmz_pokedex
+//
+//  Created by Mel Gomez on 10/10/2017.
+//  Copyright © 2017 meldarrelgomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
